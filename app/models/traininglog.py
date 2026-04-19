95dd327b1d95b1d17cd2d33828ea4e90496aea64
Mahicol <377markin@gmail.com>
2026-04-19 13:52:06 -0500

    feat: connect TrainingLog with User and Exercise objects

--- app/models/traininglog.py
+++ app/models/traininglog.py
@@ -1,5 +1,8 @@
+from user import User
+from exercise import Exercise
+
 class TrainingLog:
-    def __init__(self, usuario:str, ejercicio:str, series:int, repeticiones:str, rir:float, descanso:str, fecha:str, anotaciones:str = ('Sin anotaciones')):
+    def __init__(self, usuario:User, ejercicio:Exercise, series:int, repeticiones:str, rir:float, descanso:str, fecha:str, anotaciones:str = ('Sin anotaciones')):
         self.usuario = usuario
         self.ejercicio = ejercicio
         self.series = series
@@ -9,5 +12,4 @@ class TrainingLog:
         self.fecha = fecha 
         self.anotaciones = anotaciones
     def __str__(self):
-        return f'Bienvenido {self.usuario}\nEjercicio: \t{self.ejercicio}\nRepeticiones: \t{self.repeticiones}\nRIR: \t\t{self.rir}\nDescanso: \t{self.descanso}\nAnotaciones: \t{self.anotaciones}\nFecha: \t\t{self.fecha}'
-
+        return f'Bienvenido {self.usuario.nombre}\nEjercicio: \t{self.ejercicio.nombre}\nRepeticiones: \t{self.repeticiones}\nRIR: \t\t{self.rir}\nDescanso: \t{self.descanso}\nAnotaciones: \t{self.anotaciones}\nFecha: \t\t{self.fecha}'
