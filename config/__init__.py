602594cd43ae1503ef77c1ae960d2a13cbdee5ad
Mahicol <377markin@gmail.com>
2026-04-20 16:12:26 -0500

    fix: use environment variable for database URL

--- config/__init__.py
+++ config/__init__.py
@@ -1,7 +1,10 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres@localhost:5432/dorfin"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
